b57f1f9d6b622916ffb1ff5c32de7b1efb8db2dd
Sean <63107825+Karta060925@users.noreply.github.com>
2026-02-23 19:14:50 +0800

    update

--- js/form.js
+++ js/form.js
@@ -9,20 +9,20 @@ new Vue({
                 relation: '',
                 attend: '',
                 adults: '',
-                needCard: '',
-                adults: '',
                 children: '',
                 childSeat: '',
                 vegetarian: '',
+                needCard: '',
                 address: '',
                 message: ''
-            }
+            },
+            errors: {}
         }
     },
     watch: {
         // 👇 如果改成「不出席」，自動清空人數資料
         'form.attend'(val) {
-            if (val !== '出席') {
+            if (val !== '可以參加' || form.attend !== '我一定會參加，但是我不確定要攜幾個伴 或是隱+3 ，所以我先填表 (+80分)' || form.attend !== '我原本有事....但為了這個婚禮，決定排除萬難決定參加了！！ (+120分)') {
                 this.form.adults = '';
                 this.form.children = '';
                 this.form.childSeat = '';
@@ -31,14 +31,157 @@ new Vue({
         },
         // 👇 新增這個
         'form.needCard'(val) {
-            if (val !== '需要') {
+            if (val !== '我需要紙本喜帖收藏一輩子！') {
                 this.form.address = '';
             }
+        },
+
+        'form.name'(val) {
+            this.errors.name = val ? '' : '• 請輸入姓名'
+        },
+
+        'form.phone'(val) {
+            if (!val) {
+                this.errors.phone = '• 請輸入聯絡電話'
+            } else if (!/^09\d{8}$/.test(val)) {
+                this.errors.phone = '• 請輸入正確手機格式'
+            } else {
+                this.errors.phone = ''
+            }
+        },
+
+        'form.relation'(val) {
+            this.errors.relation = val ? '' : '• 請選擇與新人的關係'
+        },
+
+        'form.attend'(val) {
+
+            this.errors.attend = val ? '' : '• 請選擇是否出席'
+
+            if (!this.needAttendanceFields) {
+
+                this.form.adults = ''
+                this.form.children = ''
+                this.form.childSeat = ''
+                this.form.vegetarian = ''
+
+                this.errors.adults = ''
+                this.errors.children = ''
+                this.errors.childSeat = ''
+                this.errors.vegetarian = ''
+            }
+        },
+        'form.adults'(val) {
+            if (this.needAttendanceFields) {
+                this.errors.adults = val ? '' : '• 請選擇出席的成人人數'
+            }
+        },
+
+        'form.children'(val) {
+            if (this.needAttendanceFields) {
+                this.errors.children = val ? '' : '• 請選擇出席的兒童人數'
+            }
+        },
+
+        'form.childSeat'(val) {
+            if (this.needAttendanceFields) {
+                this.errors.childSeat = val ? '' : '• 請選擇兒童椅'
+            }
+        },
+
+        'form.vegetarian'(val) {
+            if (this.needAttendanceFields) {
+                this.errors.vegetarian = val ? '' : '• 請選擇素食'
+            }
+        },
+
+        'form.needCard'(val) {
+            this.errors.needCard = val ? '' : '• 請選擇需要紙本喜帖嗎？'
+        },
+
+        'form.address'(val) {
+            if (this.form.needCard === '我需要紙本喜帖收藏一輩子！') {
+                this.errors.address = val ? '' : '• 請填寫地址'
+            }
+        },
+
+        'form.message'(val) {
+            this.errors.message = val ? '' : '• 無話可說? 填了才能送出！'
+        }
+
+    },
+    computed: {
+        needAttendanceFields() {
+            return [
+                '可以參加',
+                '我一定會參加，但是我不確定要攜幾個伴 或是隱+3 ，所以我先填表 (+80分)',
+                '我原本有事....但為了這個婚禮，決定排除萬難決定參加了！！ (+120分)'
+            ].includes(this.form.attend)
         }
     },
     methods: {
+        clearError(field) {
+            this.errors[field] = ''
+        },
+        validateForm() {
+            this.errors = {}
+
+            if (!this.form.name) {
+                this.errors.name = '• 請輸入姓名'
+            }
+
+            if (!this.form.phone) {
+                this.errors.phone = '• 請輸入聯絡電話'
+            } else if (!/^09\d{8}$/.test(this.form.phone)) {
+                this.errors.phone = '• 請輸入正確手機格式'
+            }
+
+            if (!this.form.relation) {
+                this.errors.relation = '• 請選擇與新人的關係'
+            }
+
+            if (!this.form.attend) {
+                this.errors.attend = '• 請選擇是否出席'
+            }
+
+            if (this.needAttendanceFields && !this.form.adults) {
+                this.errors.adults = '• 請選擇出席的成人人數'
+            }
+
+            if (this.needAttendanceFields && !this.form.children) {
+                this.errors.children = '• 請選擇出席的兒童人數'
+            }
+
+            if (this.needAttendanceFields && !this.form.childSeat) {
+                this.errors.childSeat = '• 請選擇兒童椅'
+            }
+
+            if (this.needAttendanceFields && !this.form.vegetarian) {
+                this.errors.vegetarian = '• 請選擇素食'
+            }
+            if (!this.form.needCard) {
+                this.errors.needCard = '• 請選擇需要紙本喜帖嗎？'
+            }
+
+            if (this.form.needCard === '我需要紙本喜帖收藏一輩子！' && !this.form.address) {
+                this.errors.address = '• 請填寫地址'
+            }
+
+            if (!this.form.message) {
+                this.errors.message = '• 無話可說? 填了才能送出！'
+            }
+
+            return Object.keys(this.errors).length === 0
+        },
         submitForm() {
-            if (this.isSubmitting) return;
+            if (!this.validateForm()) {
+                this.$nextTick(() => {
+                    const el = document.querySelector('.is-invalid')
+                    if (el) el.scrollIntoView({ behavior: 'smooth', block: 'center' })
+                })
+                return
+            }
+            // if (this.isSubmitting) return;
 
             this.isSubmitting = true;
 
@@ -72,14 +215,10 @@ new Vue({
                 closeOnEsc: false,
                 closeOnClickOutside: false
             });
-
             setTimeout(() => {
                 swal.close();
                 location.reload();
             }, 2000);
-
-            this.isSubmitting = true;
-
             // 若你想送出後清空表單
             this.form = {
                 name: '',
@@ -94,6 +233,20 @@ new Vue({
                 address: '',
                 message: ''
             };
+            this.errors = {
+                name: '',
+                phone: '',
+                relation: '',
+                attend: '',
+                adults: '',
+                children: '',
+                childSeat: '',
+                vegetarian: '',
+                needCard: '',
+                address: '',
+                message: ''
+            };
+
         }
     }
 });
