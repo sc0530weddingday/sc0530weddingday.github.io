2a9905b618cb90caaf04d0d2c7cfe02de6c03f60
Sean <63107825+Karta060925@users.noreply.github.com>
2026-05-04 14:14:28 +0800

    update carousel

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAUFBko2o0GjKGk7gyf8VOLGn22NYTRlr0",
+  authDomain: "sc0530weddingday-aa817.firebaseapp.com",
+  projectId: "sc0530weddingday-aa817",
+  storageBucket: "sc0530weddingday-aa817.firebasestorage.app",
+  messagingSenderId: "827510535611",
+  appId: "1:827510535611:web:f4b7a7cf379e38961f809e",
+  measurementId: "G-6XKL8QK85Z"
+};
+
+const GITHUB_PAGES_URL = "https://sc0530weddingday.github.io/uploadImage";
